aef8de6ad49a6928e0e02acda2a40a00b44bed82
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-04-05 15:44:47 +0900

    Added save.py

--- indeed.py
+++ indeed.py
@@ -49,7 +49,7 @@ def extract_job(html):
 def extract_jobs(last_pages):
     jobs = []
     for page in range(last_pages):
-        print(f"Scrapping page {page}")
+        print(f"Scrapping indeed: Page: {page}")
         result = requests.get(f"{URL}&start={page * LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
--- so.py
+++ so.py
@@ -7,8 +7,35 @@ def get_last_page():
     request = requests.get(URL)
     soup = BeautifulSoup(request.text, "html.parser")
     pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-    print(pages)
+    last_pages = pages[-2].get_text(strip=True)
+    return int(last_pages)
+
+def extract_job(html):
+    title = html.find("h2", {"class":"fs-body3"}).find("a").string
+    company, location = html.find("h3", {"class":"fs-body1"}).find_all("span", recursive = False)
+    company = company.get_text(strip=True).strip(" \r")
+    location = location.get_text(strip=True)
+    job_id = html["data-jobid"]
+    return {
+        "title": title, 
+        "company":company, 
+        "location":location, 
+        "link":f"https://stackoverflow.com/jobs/{job_id}"
+    }
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping SO: Page: {page}")
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class":"-job"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 def get_jobs():
     last_pages = get_last_page()
+    jobs = extract_jobs(last_pages)
     return []
