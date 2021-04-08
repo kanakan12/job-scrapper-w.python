5550b313b3df38fc0af5f545174be35b8dfdabe2
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-04-08 18:08:26 +0900

    Added export

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from indeed import get_jobs
+from save import save_to_file
 
 app = Flask("Scrapper")
 
@@ -24,6 +25,21 @@ def report():
         return redirect("/")
 
     return render_template("report.html", searchingBy=word, resultNember=len(jobs), jobs=jobs)
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 app.run()
 
